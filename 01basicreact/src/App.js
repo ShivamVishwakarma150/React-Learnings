b88bf830b0cc59a4d3ebd83c4995e5a300907062
Shivam Vishwakarma <arvind.972176@gmail.com>
2024-10-23 16:53:43 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Shivam Vishwakarma</h1>
+  );
+}
+
+export default App;
---
+++ 02viteReact/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <h1>Shivam Vishwakarma</h1>
+  )
+}
+
+export default App
