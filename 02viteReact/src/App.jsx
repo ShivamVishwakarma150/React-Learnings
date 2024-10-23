3f305a689f2e0dca72a2026fd59bbc922464728b
Shivam Vishwakarma <arvind.972176@gmail.com>
2024-10-23 19:33:35 +0530

    Understanding react flow

--- 02viteReact/src/App.jsx
+++ 02viteReact/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import Coffee from './Chai'
 
 function App() {
   
   return (
+    <>
     <h1>Shivam Vishwakarma</h1>
+    <Coffee/>
+    </>
   )
 }
 
---
+++ 02viteReact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Coffee(){
+    return(
+        <h1>My Name is shivam</h1>
+    )
+}
+
+export default Coffee;
